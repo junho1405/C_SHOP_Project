be657853fd8f1d6ec6bdd8ab99de8daa31aaa80a
junho1405 <junho1405@naver.com>
2025-08-25 15:43:09 +0900

    20250825 매개변수한정자

---
+++ Program/Program.cs
@@ -0,0 +1,37 @@
+﻿
+namespace Program
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region 박싱
+            //// 각 형식의 데이터를 참조 형식으로 변환하는 과정입니다.
+            //int value = 100;
+            //object generic = value;
+
+            //Console.WriteLine("value : "+ value);
+            //Console.WriteLine("generic : " + generic);
+            #endregion
+            #region 언박싱
+            //// 참조 형식의 데이터를 값 형식으로 반환하는 과정입니다.
+            //int box = (int)generic;
+            //Console.WriteLine("box : " + box);
+            #endregion
+            utillity Utillity = new utillity();
+            Utillity.Pause();
+
+            int next = 10;
+            int previous = 0;
+
+            Utillity.Swap(ref next, ref previous);
+            Console.WriteLine("next 변수의 값 : " + next);
+            Console.WriteLine("previous 변수의 값 : " + previous);
+
+            int num = -40;
+            Utillity.Absolute(num);
+            Console.WriteLine("Absolute의 값 : " + Utillity.Absolute(in num));
+        }
+        
+    }
+}
---
+++ Program/utillity.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Schema;
+
+namespace Program
+{
+    internal class utillity
+    {
+        private int data;
+        public void Pause()
+        {
+            Console.WriteLine("Pause....");
+        }
+        public void Swap(ref int x, ref int y)
+        {
+            int temporary = x;
+            x = y;
+            y = temporary;
+            //Swap(ref x, ref y);
+        }
+        public int Absolute(in int value)
+        {
+            return value < 0 ? -value : value;
+            //if (value < 0) Console.WriteLine("절대값 : " + (value)*-1);
+            //if (value > 0) Console.WriteLine("절대값 : " + value);
+
+        }
+    }
+}
